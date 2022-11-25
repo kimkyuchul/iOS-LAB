131d8d921af037ffa0ecf0b081271bff974da4a5
김규철 <rbcjf122@gmail.com>
2022-11-25 15:50:20 +0900

    Edit : URLConst 작성

---
+++ MoyaPractice/MoyaPractice/Network/URLConst.swift
@@ -0,0 +1,17 @@
+//
+//  URLConst.swift
+//  MoyaPractice
+//
+//  Created by 김규철 on 2022/11/25.
+//
+
+import Foundation
+
+enum URLConst {
+    // MARK: - baseURL
+    static let base = "https://api.bithumb.com/public"
+    
+    // MARK: - 현재가 조회
+    static let ticker = "/ticker"
+
+}
